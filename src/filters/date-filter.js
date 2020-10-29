9800e1a4db7de721b8f3a0936635780190e3d68b
Isabel Costa <11148726+isabelcosta@users.noreply.github.com>
2020-10-29 20:16:13 +0000

    blog: make blog list items inline (#47)

--- src/filters/date-filter.js
+++ src/filters/date-filter.js
@@ -10,7 +10,10 @@ module.exports = function dateFilter(value) {
 
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   const dayWithSuffix = appendSuffix(dateObject.getDate());
+  
+  // custom style
+  const customMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
   const dayWithoutSuffix = dateObject.getDate();
 
-  return `${dayWithoutSuffix} ${months[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
+  return `${dayWithoutSuffix} ${customMonths[dateObject.getMonth()]} ${dateObject.getFullYear()}`;
 };
